179644700e2cfd5ff063cca02cf9cae17cafcd94
gautamprashant <gautamprashant184@gmail.com>
2024-11-09 08:34:28 +0545

    add js

---
+++ first.js
@@ -0,0 +1,14 @@
+const product ={
+    title:"ball pen",
+    rating:4,
+    offer:8,
+    price:270,
+};
+console.log(product);
+const profile={
+    userName:"prashnt Gautam",
+    isFollow:false,
+    followers:23445,
+    folloeing:934,
+};
+console.log("profile");
